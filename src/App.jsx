98ce218d856412e344c77f8c43296c5e2f1670d0
faima-ahmed <faimaahmeddiya@gmail.com>
2024-12-30 11:22:33 +0600

    Commit 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProductsView from "./features/products/ProductsView";
+
+const App = () => {
+    return (
+        <div>
+           <ProductsView/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/features/products/ProductsView.jsx
@@ -0,0 +1,9 @@
+const ProductsView = () => {
+    return (
+        <div>
+            List of Products
+        </div>
+    );
+};
+
+export default ProductsView;
